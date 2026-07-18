79b0b8e9e6d227f6223909bc1a053ace08c17873
Gorbunov Danil <danil45@bk.ru>
2026-07-18 10:42:40 +0500

    topic_7, tasks: 0, 1, 2

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,1 +1,14 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+number: int = int(input())
+print(f"Таблица умножения для числа {number} с помощью цикла for:")
+
+for i in range (1, 11, 1):
+    result = number * i
+    print(f"{number} * {i} = {result}")
+
+
+print(f"Таблица умножения для числа {number} с помощью цикла while:")
+j: int = 0
+while j < 10:
+    j += 1
+    result_2 = number * j
+    print(f"{number} * {j} = {result_2}")
