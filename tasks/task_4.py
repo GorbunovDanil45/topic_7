4df7828da784b586ab08b9258f864aaa43d4e710
Gorbunov Danil <danil45@bk.ru>
2026-07-18 15:20:33 +0500

    topic_7, tasks: 3, 4, 5, 6

--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,1 +1,17 @@
 # TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+number: int = int(input('Введите число:'))
+
+if number < 1:
+    print('Число должно быть больше или равно 1')
+
+elif number == 1:
+    print('1')
+
+else:
+    count: int = 0
+    result: int = 0
+    while count < number:
+        count += 1
+        result += count
+
+    print(f'Сумма всех чисел от 1 до {number} будет равна: {result}')
--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,1 +1,15 @@
 # TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+number: int = int(input('Введите целое число:'))
+absolute_value = abs(number)
+
+if absolute_value == 0:
+        count = 1
+
+else:
+    count: int = 0
+    while absolute_value != 0:
+        new_num = absolute_value % 10
+        absolute_value = absolute_value // 10
+        count += 1
+
+print(f"Количество цифр в числе: {count}")
